d7aff52e5476800e86498ff19f6ee7c4f7b1c1b0
mariusz <mariusz@kuchta.dev>
2025-11-14 21:54:38 +0200

    initial mobile app code

---
+++ saunsei/app/(tabs)/index.tsx
@@ -0,0 +1,39 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet, View } from 'react-native';
+
+import { HelloWave } from '@/components/hello-wave';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { Link } from 'expo-router';
+
+export default function HomeScreen() {
+  return (
+      <View>
+        <ThemedText style={styles.center} type="subtitle">Sensei go</ThemedText>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  center: {
+      textAlign: 'center',
+      marginTop: 160
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
